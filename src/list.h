13e70a9e93bddeb54c298be45b3da958fde9370c
zyearn <zhujiashun2010@gmail.com>
2015-04-21 18:54:56 +0800

    add list.h

---
+++ src/list.h
@@ -0,0 +1,80 @@
+#ifndef _ZV_LIST_H
+#define _ZV_LIST_H
+
+#define NULL 0
+/*
+* list_head, reference linux kernel implementation
+*
+*/
+
+struct list_head {
+    struct list_head *prev, *next;
+};
+
+#define INIT_LIST_HEAD(ptr) do {\
+    struct list_head *_ptr = (struct list_head *)ptr;   \
+    (_ptr)->next = (_ptr); (_ptr->prev) = (_ptr);       \
+} while(0)
+
+/*
+* Insert a new entry to two consecutive entries.
+*/
+static inline void __list_add(struct list_head *_new, struct list_head *prev, struct list_head *next) {
+    _new->next = next;
+    next->prev = _new;
+    prev->next = _new;
+    _new->prev = prev;
+}
+
+static inline void list_add(struct list_head *_new, struct list_head *head) {
+    __list_add(_new, head, head->next);
+}
+
+static inline void list_add_tail(struct list_head *_new, struct list_head *head) {
+    __list_add(_new, head, head->next);
+}
+
+/*
+* Delete a entry to two consecutive entries.
+*/
+static inline void __list_del(struct list_head *prev, struct list_head *next) {
+    prev->next = next;
+    next->prev = prev;
+}
+
+static inline void list_del(struct list_head *entry) {
+    __list_del(entry->prev, entry->next);
+    entry->next = entry->prev = NULL;
+}
+
+/*
+*   check whether the list is empty
+*/
+static inline int list_empty(struct list_head *head) {
+    return (head->next == head) && (head->prev == head);
+}
+
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+
+#define container_of(ptr, type, member) ({                      \
+        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+        (type *)( (char *)__mptr - offsetof(type,member) );     \
+})
+
+#define list_entry(ptr, type, member) \
+	container_of(ptr, type, member)
+
+#define list_for_each(pos, head) \
+	for (pos = (head)->next; pos != (head); pos = pos->next)
+
+#define list_for_each_prev(pos, head) \
+    for (pos = (head)->prev; pos != (head); pos = pos->prev)
+
+/*
+#define list_for_each_entry(pos, head, member)  \
+    for (pos = list_entry((head)->next, typeof(*pos), member);  \
+        &(pos->member) != (head);                               \
+        pos = list_entry(pos->member.next, typeof(*pos), member))
+*/
+
+#endif 
---
+++ tests/list_test.c
@@ -0,0 +1,67 @@
+#include <list.h>
+#include <dbg.h>
+
+typedef struct list_head list_head;
+
+typedef struct {
+    void *arg;
+    list_head list;  
+} st_header;
+
+typedef struct {
+    void *arg1;
+    void *arg2;
+    void *arg3;
+    list_head list;
+} st_struct;
+
+void add_node(void *arg, list_head *head) {
+    st_struct *s = (st_struct *)malloc(sizeof(st_struct));
+    s->arg1 = arg;
+    s->arg2 = arg;
+    s->arg3 = arg;
+    list_add(&(s->list), head);
+}
+
+void del_node(list_head *entry) {
+    list_del(entry);
+    st_struct *s = list_entry(entry, st_struct, list);
+    free(s);
+}
+
+void display(list_head *head) {
+    list_head *pos;
+    st_struct *et;
+
+    list_for_each(pos, head) {
+        et = list_entry(pos, st_struct, list);
+        printf("arg = %d\n", (int)et->arg1);
+    }
+}
+
+int main (int argc, char *argv[]) {
+    st_header st_h;
+    INIT_LIST_HEAD(&st_h.list);
+
+    int isEmpty = list_empty(&st_h.list);
+    check(isEmpty == 1, "list should be empty, isEmpty = %d", isEmpty);
+
+    int i;
+    for (i=0; i<100; i++) {
+        add_node((void *)i, &(st_h.list));
+    }
+    
+    isEmpty = list_empty(&(st_h.list));
+    check(isEmpty == 0, "list should not be empty, isEmpty = %d", isEmpty);
+    
+    printf("before del:\n");
+    display(&(st_h.list));
+
+    for (i=0; i<32; i++) {
+        del_node(st_h.list.next);
+    }
+
+    printf("after del:\n");
+    display(&(st_h.list));
+    return 0;
+}
